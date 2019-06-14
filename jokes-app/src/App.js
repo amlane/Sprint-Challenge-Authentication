0a7fb2f60074951409cf0f8e86353af1c7ef5f96
amlane <twelvexstring@gmail.com>
2019-06-14 11:17:30 -0500

    basic stretch completed, no styling

--- jokes-app/src/App.js
+++ jokes-app/src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
+import { Route, NavLink, withRouter } from "react-router-dom";
+
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import Jokes from "./components/Jokes";
+
 import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dad Jokes</h1>
-    </div>
-  );
+class App extends React.Component {
+  logout = e => {
+    e.preventDefault();
+
+    localStorage.removeItem("token");
+    this.props.history.push("/signin");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <h1>Dad Jokes</h1>
+          <NavLink to="/signup">Sign Up</NavLink>{" "}
+          <NavLink to="/signin">Sign In</NavLink>{" "}
+          <NavLink to="/jokes">Jokes</NavLink>
+          <button onClick={this.logout}>Log Out</button>
+        </nav>
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/jokes" component={Jokes} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
---
+++ jokes-app/src/components/auth/SignIn.js
@@ -0,0 +1,68 @@
+import React from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+
+import "../../styles/App.css";
+
+class SignIn extends React.Component {
+  state = {
+    username: "amanda",
+    password: "123"
+  };
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:3300/api/login", this.state)
+      .then(res => {
+        // console.log(res);
+        localStorage.setItem("token", res.data.token);
+        this.props.history.push("/jokes");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <header className="App-header">
+        <form onSubmit={this.handleSubmit} className="login-form">
+          <h2>Log In</h2>
+
+          <label>username</label>
+          <input
+            value={this.state.username}
+            type="text"
+            required
+            name="username"
+            onChange={this.handleInput}
+          />
+
+          <label>password</label>
+          <input
+            value={this.state.password}
+            type="password"
+            required
+            name="password"
+            onChange={this.handleInput}
+          />
+
+          <button type="submit">Sign In</button>
+          <p className="new-user-signup">
+            Not a User? <NavLink to="/signup">Sign Up</NavLink>
+          </p>
+        </form>
+      </header>
+    );
+  }
+}
+
+export default SignIn;
