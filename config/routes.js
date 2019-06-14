8b0fa57d659146c6c599334cdffd7cd7e8177d6c
amlane <twelvexstring@gmail.com>
2019-06-14 10:34:17 -0500

    added login and tested in postman. I got jokes!

--- config/routes.js
+++ config/routes.js
@@ -34,6 +34,26 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // generate token
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token //return the token upon login
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
